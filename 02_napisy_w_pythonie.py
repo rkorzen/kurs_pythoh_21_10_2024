915b0a5a438fece02734bae2b881178346d8df61
rkorzen <korzeniewski@gmail.com>
2024-10-21 12:05:15 +0200

    zadanie 01

---
+++ 02_napisy_w_pythonie.py
@@ -0,0 +1,79 @@
+
+print("to jest napis")
+print("to też 'jest' napis")
+print('to też "jest" napis')
+print("to też \"jest\" napis")
+
+print("to jest napis\nz nowa\t linia")
+print("to jest napis\\nz nowa\\t linia")
+print(r"to jest napis\nz nowa\t linia")
+
+
+print("""
+1 linia 
+2 linia
+""")
+
+
+print('''
+1 linia 
+2 linia
+''')
+
+x = (
+    "ala"
+    "ma"
+    "kota"
+)
+
+x2 = "ala" \
+    "ma" \
+    "kota"
+
+print(x)
+print(x2)
+
+napis1 = "Play"
+napis2 = "ma ciekawą ofertę"
+x = 10
+print(napis1 + " " + napis2)
+
+print("%s %s %.2f" % (napis1, napis2, 10))
+print("%s %s %d" % (napis1, napis2, 10.2))
+
+print("a {} {} {:.2f}"
+      .format(napis1, napis2, x)
+      .upper()
+)
+
+print(f"{napis1} {napis2} {x:.2f}")
+
+print(napis1, napis2, sep="---")
+
+# help(print)
+# print(xxx)
+
+
+print("=" * 80)
+
+print("ala" == "ala")
+print("pies" < "ries")
+
+
+print(str(dir))
+print(dir)
+
+print("aax" in "aaax")
+
+print("bbbbbbaaax".index("aax"))
+
+
+for znak in "uhsfu9fdhbver":
+    print(znak, end=":")
+
+
+print()
+#        012345678910
+napis = "ala ma kota"
+
+print(napis[0])
---
+++ cwiczenia/cwiczenie_01.py
@@ -0,0 +1,20 @@
+"""
+1. stworz zestaw danycgh opisujacy 3 pracownikoiw
+
+imie, wiek, stawka
+
+
+2. stworz zmienna ktora bedzie przechowywal "raport" o tych 3 osobach
+
+3. uzyj print na tej zmiennej. Oczekiwany wynik to:
+
+
+imie     wiek stawka
+====================
+Jan      32   200.00
+Gabrysia 25   185.00
+Zofia    33   220.24
+
+"""
+x, y = 1, 2
+print(f"{x:<5} {y:6}")
