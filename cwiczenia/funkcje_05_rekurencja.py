b1d6f106a098733e75d5573faad16b9e324bd4ba
rkorzen <korzeniewski@gmail.com>
2024-10-22 16:28:06 +0200

    end day 2

--- cwiczenia/funkcje_05_rekurencja.py
+++ cwiczenia/funkcje_05_rekurencja.py
@@ -2,8 +2,29 @@
 Zaimplementuj funkcję spłaszczającą podaną listę.
 Przykład użycia:
 
->>> splaszcz([1, 2, 3, [4, 5, [6]], 7])
+splaszcz([1, 2, 3, [4, 5, [6]], 7])
 
 [1, 2, 3, 4, 5, 6, 7]
 
 """
+
+
+def splaszcz(lista):
+    wynik = []
+    for el in lista:
+        # if type(el) == list:
+        if isinstance(el, list):
+            wynik.extend(splaszcz(el))
+        else:
+            wynik.append(el)
+
+    return wynik
+
+
+def test_splaszcz():
+    assert splaszcz([]) == []
+    assert splaszcz([1, 2, 3]) == [1, 2, 3]
+    assert splaszcz([1, [2, 3]]) == [1, 2, 3]
+    assert splaszcz([1, [2, [3]], 4]) == [1, 2, 3, 4]
+
+
