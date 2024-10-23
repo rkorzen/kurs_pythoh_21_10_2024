180ab4955db45e83504fe4d0b5a38bb3353260b1
rkorzen <korzeniewski@gmail.com>
2024-10-23 12:20:39 +0200

    csv2

---
+++ modul_random.py
@@ -0,0 +1,7 @@
+import random
+
+print(random.randint(1, 100))
+
+osoby = ["MK", "WM", "PM", "KM", "KJ", "SS", "SP", "DB", "WM", "KMich", "SPio", "CM", "KP", "WD", "MM"]
+
+print(random.choice(osoby))
